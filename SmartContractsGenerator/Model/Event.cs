b50df224605f5a0d758121ae157cf18df450789a
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-02-29 15:17:57 +0100

    Basic model's objects

---
+++ SmartContractsGenerator/Model/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartContractsGenerator.Model
+{
+    class Event
+    {
+        public string Name { get; set; }
+    }
+}
