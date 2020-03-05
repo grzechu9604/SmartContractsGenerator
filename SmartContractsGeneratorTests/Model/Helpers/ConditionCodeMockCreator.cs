b95b5649bdc0fde2d455d5a052597ce381f9b8a7
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-03-05 21:00:00 +0100

    IfStatement

---
+++ SmartContractsGeneratorTests/Model/Helpers/ConditionCodeMockCreator.cs
@@ -0,0 +1,30 @@
+﻿using SmartContractsGenerator.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartContractsGeneratorTests.Model.Helpers
+{
+    class ConditionCodeMockCreator :IDisposable
+    {
+        private readonly MocksHolder mocksHolder = new MocksHolder();
+
+        public void Dispose()
+        {
+            mocksHolder.Dispose();
+        }
+
+        public Condition PrepareMock(string expectedCode)
+        {
+            var mock = mocksHolder.GetMock();
+
+            mock.Mock<Condition>()
+                .Setup(x => x.GenerateCode())
+                .Returns(expectedCode);
+
+            var preparedMock = mock.Create<Condition>();
+
+            return preparedMock;
+        }
+    }
+}
---
+++ SmartContractsGeneratorTests/Model/Helpers/InstructionsListMockCreator.cs
@@ -0,0 +1,38 @@
+﻿using SmartContractsGenerator.Model.AbstractPatterns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartContractsGeneratorTests.Model.Helpers
+{
+    class InstructionsListMockCreator : IDisposable
+    {
+        private readonly MocksHolder mocksHolder = new MocksHolder();
+
+        public void Dispose()
+        {
+            mocksHolder.Dispose();
+        }
+
+        public InstructionsList PrepareMock(string expectedCode, bool containsOnlyIf, bool containsAnyElement)
+        {
+            var mock = mocksHolder.GetMock();
+
+            mock.Mock<InstructionsList>()
+                .Setup(x => x.GenerateCode())
+                .Returns(expectedCode);
+
+            mock.Mock<InstructionsList>()
+                .Setup(x => x.ContainsOnlyIf())
+                .Returns(containsOnlyIf);
+
+            mock.Mock<InstructionsList>()
+                .Setup(x => x.Any())
+                .Returns(containsAnyElement);
+
+            var preparedMock = mock.Create<InstructionsList>();
+
+            return preparedMock;
+        }
+    }
+}
