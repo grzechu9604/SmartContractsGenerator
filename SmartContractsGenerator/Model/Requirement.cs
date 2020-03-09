95ddc0f68d1df87c37d9c8dbc2de461c3f033a8a
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-03-09 22:26:29 +0100

    Requirement

--- SmartContractsGenerator/Model/Requirement.cs
+++ SmartContractsGenerator/Model/Requirement.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using SmartContractsGenerator.Exceptions;
+using SmartContractsGenerator.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SmartContractsGenerator.Model
 {
-    class Requirement
+    public class Requirement : ICodeGenerator
     {
+        public Condition Condition { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public virtual string GenerateCode()
+        {
+            if (Condition == null)
+            {
+                throw new MissingMandatoryElementException("Condition is mandatory element of requirement");
+            }
+
+            if (!string.IsNullOrWhiteSpace(ErrorMessage))
+            {
+                return $"require({Condition.GenerateCode()}, {ErrorMessage});\n";
+            }
+            else
+            {
+                return $"require({Condition.GenerateCode()});\n";
+            }
+        }
     }
 }
