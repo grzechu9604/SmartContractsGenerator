7ed72dcb3a316e9d8b3dbdbef4a0ab6c3b497452
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-03-09 19:31:29 +0100

    Names validator

---
+++ SmartContractsGenerator/Validators/NameValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SmartContractsGenerator.Validators
+{
+    public sealed class NameValidator
+    {
+        private static readonly Regex regex = new Regex(@"[a-zA-Z_]\w*");
+        public static bool IsValidName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return false;
+            }
+            else
+            {
+                var match = regex.Match(name);
+                return match.Success && match.Length == name.Length && !IsInBannedWordsList(name);
+            }
+        }
+
+        private static bool IsInBannedWordsList(string name)
+        {
+            //TODO Add solidity keywords list
+            return false;
+        }
+    }
+}
