bd1e9df2ff7081935781022ec0af47455b542919
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-03-31 11:28:19 +0200

    Add TODO

--- SmartContractsGenerator/Model/Call.cs
+++ SmartContractsGenerator/Model/Call.cs
@@ -4,6 +4,7 @@ using SmartContractsGenerator.Model.AbstractPatterns;
 
 namespace SmartContractsGenerator.Model
 {
+    //TODO: function call should aslo have possibility to be assigned!!! - IAssignable interface
     public class Call : Instruction
     {
         public ICallable Callable { get; set; }
