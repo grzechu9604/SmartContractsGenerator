be71e4352aa7798fe0c6bb8e63bf0886e593c20e
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-03-31 19:28:27 +0200

    Missing Tests

---
+++ SmartContractsGeneratorTests/Model/AssignmentTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartContractsGenerator.Exceptions;
+using SmartContractsGenerator.Model;
+using SmartContractsGeneratorTests.Model.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartContractsGenerator.Model.Tests
+{
+    [TestClass()]
+    public class AssignmentTests
+    {
+        private static readonly VariableMocksCreator variableMocksCreator = new VariableMocksCreator();
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            variableMocksCreator.Dispose();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(MissingMandatoryElementException))]
+        public void EmptyAssignmentTest()
+        {
+            new Assignment().GenerateCode();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(MissingMandatoryElementException))]
+        public void EmptyAssignmentTest2()
+        {
+            new Assignment()
+            {
+                Destination = variableMocksCreator.PrepareMock("TEST", "TEST2")
+            }.GenerateCode();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(MissingMandatoryElementException))]
+        public void EmptyAssignmentTest3()
+        {
+            new Assignment()
+            {
+                Source = variableMocksCreator.PrepareMock("TEST", "TEST2")
+            }.GenerateCode();
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetDataForGenerateCodeTest), DynamicDataSourceType.Method)]
+        public void GenerateCodeTest(Assignment a, string expected)
+        {
+            System.Diagnostics.Contracts.Contract.Requires(a != null);
+            Assert.AreEqual(expected, a.GenerateCode());
+        }
+
+        static IEnumerable<object[]> GetDataForGenerateCodeTest()
+        {
+            var v1 = "VARIABLE 1";
+            var v2 = "VARIABLE 2";
+            var a1 = new Assignment()
+            {
+                Destination = variableMocksCreator.PrepareMock(v1, null),
+                Source = variableMocksCreator.PrepareMock(v2, null)
+            };
+            yield return new object[] { a1, $"{v1} = {v2}" };
+
+            var a2 = new Assignment()
+            {
+                Destination = variableMocksCreator.PrepareMock(v2, null),
+                Source = variableMocksCreator.PrepareMock(v1, null)
+            };
+            yield return new object[] { a2, $"{v2} = {v1}" };
+        }
+    }
+}
---
+++ SmartContractsGeneratorTests/Model/DeclarationTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartContractsGenerator.Exceptions;
+using SmartContractsGeneratorTests.Model.Helpers;
+using System.Collections.Generic;
+
+namespace SmartContractsGenerator.Model.Tests
+{
+    [TestClass()]
+    public class DeclarationTests
+    {
+        private static readonly VariableMocksCreator variableMocksCreator = new VariableMocksCreator();
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            variableMocksCreator.Dispose();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(MissingMandatoryElementException))]
+        public void EmptyDeclarationTest()
+        {
+            new Declaration().GenerateCode();
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetDataForGenerateCodeTest), DynamicDataSourceType.Method)]
+        public void GenerateCodeTest(Declaration d, string expected)
+        {
+            System.Diagnostics.Contracts.Contract.Requires(d != null);
+            Assert.AreEqual(expected, d.GenerateCode());
+        }
+
+        static IEnumerable<object[]> GetDataForGenerateCodeTest()
+        {
+            var d1 = "DECLARATION 1";
+            yield return new object[] { new Declaration() { Variable = variableMocksCreator.PrepareMock(null, d1) }, d1 };
+            var d2 = "DECLARATION 2";
+            yield return new object[] { new Declaration() { Variable = variableMocksCreator.PrepareMock(null, d2) }, d2 };
+        }
+    }
+}
