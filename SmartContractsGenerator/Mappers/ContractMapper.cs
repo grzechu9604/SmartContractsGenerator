6d9a9ae90211715c8993db79d3650fa948b43d99
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-04-15 11:06:44 +0200

    Mapping constructor with visibility

--- SmartContractsGenerator/Mappers/ContractMapper.cs
+++ SmartContractsGenerator/Mappers/ContractMapper.cs
@@ -1,4 +1,5 @@
 ﻿using SmartContractsGenerator.Model;
+using SmartContractsGenerator.Model.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,13 +31,42 @@ namespace SmartContractsGenerator.Mappers
         {
             Contract c = new Contract();
 
-            var nameNode = node.SelectSingleNode("gxml:field", nsmgr);
+            var nameNode = node.SelectSingleNode("gxml:field[@name=\"Name\"]", nsmgr);
             if (nameNode != null)
             {
                 c.Name = nameNode.InnerText;
             }
 
+            var constructorNode = node.SelectSingleNode("gxml:statement[@name=\"Constructor\"]/gxml:block[@type=\"contract_constructor\"]", nsmgr);
+            if (constructorNode != null)
+            {
+                c.Constructor = GetConstructorFromXmlNode(constructorNode, nsmgr);
+            }
+
             return c;
+        }
+
+        public Constructor GetConstructorFromXmlNode(XmlNode node, XmlNamespaceManager nsmgr)
+        {
+            Constructor c = new Constructor();
+
+            var visibilityNode = node.SelectSingleNode("gxml:field[@name=\"Visibility\"]", nsmgr);
+            if (visibilityNode != null)
+            {
+                c.Visibility = GetVisibilityFromXmlNode(visibilityNode);
+            }
+
+            return c;
+        }
+
+        public Visibility? GetVisibilityFromXmlNode(XmlNode node)
+        {
+            if (node.InnerText.All(c => char.IsDigit(c)))
+            {
+                return (Visibility)Convert.ToInt32(node.InnerText);
+            }
+
+            return null;
         }
     }
 }
