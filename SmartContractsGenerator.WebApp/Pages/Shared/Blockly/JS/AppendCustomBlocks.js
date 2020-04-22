fe0ac3abb2ed34f4c05b1bb392bc9e4a01961e2c
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-04-22 08:26:15 +0200

    Variable can be used in operation

--- SmartContractsGenerator.WebApp/Pages/Shared/Blockly/JS/AppendCustomBlocks.js
+++ SmartContractsGenerator.WebApp/Pages/Shared/Blockly/JS/AppendCustomBlocks.js
@@ -181,13 +181,13 @@ Blockly.Blocks['operation'] = {
         this.appendDummyInput()
             .appendField("Operation");
         this.appendValueInput("left_side")
-            .setCheck("operation")
+            .setCheck(["variable", "operation"])
             .appendField("Left");
         this.appendDummyInput()
             .appendField("Operator")
             .appendField(new Blockly.FieldDropdown([["+", "0"], ["-", "1"], ["%", "2"], ["/", "3"], ["*", "4"], ["!", "5"], ["||", "6"], ["&&", "7"], ["^", "8"], ["==", "9"], ["!=", "10"]]), "Operator");
         this.appendValueInput("right_side")
-            .setCheck("operation")
+            .setCheck(["variable", "operation"])
             .appendField("Right");
         this.setOutput(true, "operation");
         this.setColour(230);
