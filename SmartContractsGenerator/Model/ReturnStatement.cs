ddf9ebc6299148e1f2547bf0aede5ec4f40dcf9f
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-05-06 18:49:06 +0200

    Return statement in model

---
+++ SmartContractsGenerator/Model/ReturnStatement.cs
@@ -0,0 +1,20 @@
+﻿using SmartContractsGenerator.Exceptions;
+using SmartContractsGenerator.Interfaces;
+
+namespace SmartContractsGenerator.Model
+{
+    public class ReturnStatement : IInstruction
+    {
+        public IValueContainer ToReturn { get; set; }
+
+        public string GenerateCode()
+        {
+            if (ToReturn == null)
+            {
+                throw new MissingMandatoryElementException("Object to return is mandatory element of return statement");
+            }
+
+            return $"return {ToReturn.GenerateCode()}";
+        }
+    }
+}
