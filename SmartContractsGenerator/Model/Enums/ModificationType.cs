3e382098cee49ef4b1483891a768cd1547df7167
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-05-08 16:07:46 +0200

    Modification type (pure/view) objects in model

---
+++ SmartContractsGenerator/Model/Enums/ModificationType.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartContractsGenerator.Model.Enums
+{
+    public enum ModificationType
+    {
+        // Read/Write
+        None = 0,
+        // Read only
+        View = 1,
+        // neither read nor write
+        Pure = 2
+    }
+}
---
+++ SmartContractsGeneratorTests/Model/Enums/ModificationTypeExtenstionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SmartContractsGenerator.Model.Enums.Tests
+{
+    [TestClass()]
+    public class ModificationTypeExtenstionTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(GetDataForTests), DynamicDataSourceType.Method)]
+        public void GenerateCodeTest(ModificationType modificationType, string expected)
+        {
+            Assert.AreEqual(expected, modificationType.GenerateCode());
+        }
+
+        static IEnumerable<object[]> GetDataForTests()
+        {
+            var appropriateCode = new Dictionary<ModificationType, string>()
+            {
+                { ModificationType.None, null },
+                { ModificationType.View, "view" },
+                { ModificationType.Pure, "pure" }
+            };
+
+            foreach (var entry in appropriateCode)
+            {
+                yield return new object[] { entry.Key, entry.Value };
+            }
+        }
+    }
+}
