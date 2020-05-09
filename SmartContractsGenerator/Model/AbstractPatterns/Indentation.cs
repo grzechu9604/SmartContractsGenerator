42112c885e50151c801c98e22c237c1aba3a9155
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-05-09 14:44:56 +0200

    Indentation class

---
+++ SmartContractsGenerator/Model/AbstractPatterns/Indentation.cs
@@ -0,0 +1,14 @@
+﻿namespace SmartContractsGenerator.Model.AbstractPatterns
+{
+    public class Indentation
+    {
+        private int IndentationLevel = 0;
+
+        public void IncrementIndentationLevel() => IndentationLevel++;
+
+        public string GenerateCode()
+        {
+            return new string('\t', IndentationLevel);
+        }
+    }
+}
---
+++ SmartContractsGeneratorTests/Model/AbstractPatterns/IndentationTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SmartContractsGenerator.Model.AbstractPatterns.Tests
+{
+    [TestClass()]
+    public class IndentationTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(GetDataForTests), DynamicDataSourceType.Method)]
+        public void GenerateCodeTest(Indentation indentation, string expected)
+        {
+            System.Diagnostics.Contracts.Contract.Requires(indentation != null);
+            Assert.AreEqual(expected, indentation.GenerateCode());
+        }
+        static IEnumerable<object[]> GetDataForTests()
+        {
+            var indentation = new Indentation();
+            yield return new object[] { indentation, string.Empty };
+
+            for (int i = 1; i < 10; i++)
+            {
+                indentation.IncrementIndentationLevel();
+                yield return new object[] { indentation, new string('\t', i) };
+            }
+        }
+    }
+}
