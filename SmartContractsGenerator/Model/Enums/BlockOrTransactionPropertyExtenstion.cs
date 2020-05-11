025d3930c6665a4d5b358d68422da9ad5fc404c7
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-05-11 22:38:52 +0200

    Block and transaction property in model

---
+++ SmartContractsGenerator/Model/Enums/BlockOrTransactionPropertyExtenstion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SmartContractsGenerator.Model.Enums
+{
+    public static class BlockOrTransactionPropertyExtenstion
+    {
+        private static readonly Dictionary<BlockOrTransactionProperty, string> AppropriateCode = new Dictionary<BlockOrTransactionProperty, string>()
+        {
+            { BlockOrTransactionProperty.BlockCoinbase, "block.coinbase" },
+            { BlockOrTransactionProperty.BlockDifficulty, "block.difficulty" },
+            { BlockOrTransactionProperty.BlockGaslimit, "block.gaslimit" },
+            { BlockOrTransactionProperty.BlockNumber, "block.number" },
+            { BlockOrTransactionProperty.BlockTimestamp, "block.timestamp" },
+            { BlockOrTransactionProperty.Gasleft, "gasleft()" },
+            { BlockOrTransactionProperty.MsgData, "msg.data" },
+            { BlockOrTransactionProperty.MsgSender, "msg.sender" },
+            { BlockOrTransactionProperty.MsgSig, "msg.sig" },
+            { BlockOrTransactionProperty.MsgValue, "msg.value" },
+            { BlockOrTransactionProperty.Now, "now" },
+            { BlockOrTransactionProperty.TxGasprice, "tx.gasprice" },
+            { BlockOrTransactionProperty.TxOrigin, "tx.origin" }
+        };
+
+        public static string GenerateCode(this BlockOrTransactionProperty property)
+        {
+            return AppropriateCode[property];
+        }
+    }
+}
---
+++ SmartContractsGeneratorTests/Model/Enums/BlockOrTransactionPropertyExtenstionTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SmartContractsGenerator.Model.Enums.Tests
+{
+    [TestClass()]
+    public class BlockOrTransactionPropertyExtenstionTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(GetDataForTests), DynamicDataSourceType.Method)]
+        public void GenerateCodeTest(BlockOrTransactionProperty property, string expected)
+        {
+            Assert.AreEqual(expected, property.GenerateCode());
+        }
+
+        static IEnumerable<object[]> GetDataForTests()
+        {
+            var appropriateCode = new Dictionary<BlockOrTransactionProperty, string>()
+            {
+                { BlockOrTransactionProperty.BlockCoinbase, "block.coinbase" },
+                { BlockOrTransactionProperty.BlockDifficulty, "block.difficulty" },
+                { BlockOrTransactionProperty.BlockGaslimit, "block.gaslimit" },
+                { BlockOrTransactionProperty.BlockNumber, "block.number" },
+                { BlockOrTransactionProperty.BlockTimestamp, "block.timestamp" },
+                { BlockOrTransactionProperty.Gasleft, "gasleft()" },
+                { BlockOrTransactionProperty.MsgData, "msg.data" },
+                { BlockOrTransactionProperty.MsgSender, "msg.sender" },
+                { BlockOrTransactionProperty.MsgSig, "msg.sig" },
+                { BlockOrTransactionProperty.MsgValue, "msg.value" },
+                { BlockOrTransactionProperty.Now, "now" },
+                { BlockOrTransactionProperty.TxGasprice, "tx.gasprice" },
+                { BlockOrTransactionProperty.TxOrigin, "tx.origin" }
+            };
+
+            foreach (var entry in appropriateCode)
+            {
+                yield return new object[] { entry.Key, entry.Value };
+            }
+        }
+    }
+}
