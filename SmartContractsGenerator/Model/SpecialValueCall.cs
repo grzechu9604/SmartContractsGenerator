2986b04c8c40146e97c491d1a2ed2fb39eb723dc
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-05-11 23:00:53 +0200

    Special value call object

---
+++ SmartContractsGenerator/Model/SpecialValueCall.cs
@@ -0,0 +1,22 @@
+﻿using SmartContractsGenerator.Exceptions;
+using SmartContractsGenerator.Interfaces;
+using SmartContractsGenerator.Model.Enums;
+using System;
+
+namespace SmartContractsGenerator.Model
+{
+    public class SpecialValueCall : ICodeGenerator, IInstruction
+    {
+        public BlockOrTransactionProperty? PropertyToCall { get; set; }
+        
+        public string GenerateCode()
+        {
+            if (PropertyToCall == null)
+            {
+                throw new MissingMandatoryElementException("PropertyToCall is obligatory element of SpecialValueCall");
+            }
+
+            return PropertyToCall.Value.GenerateCode();
+        }
+    }
+}
---
+++ SmartContractsGeneratorTests/Model/SpecialValueCallTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartContractsGenerator.Exceptions;
+using SmartContractsGenerator.Model;
+using SmartContractsGenerator.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartContractsGenerator.Model.Tests
+{
+    [TestClass()]
+    public class SpecialValueCallTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(MissingMandatoryElementException))]
+        public void EmptyAssignmentTest()
+        {
+            new SpecialValueCall().GenerateCode();
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(GetDataForGenerateCodeTest), DynamicDataSourceType.Method)]
+        public void GenerateCodeTest(SpecialValueCall svc, string expected)
+        {
+            System.Diagnostics.Contracts.Contract.Requires(svc != null);
+            Assert.AreEqual(expected, svc.GenerateCode());
+        }
+
+        static IEnumerable<object[]> GetDataForGenerateCodeTest()
+        {
+            var appropriateCode = new Dictionary<BlockOrTransactionProperty, string>()
+            {
+                { BlockOrTransactionProperty.BlockCoinbase, "block.coinbase" },
+                { BlockOrTransactionProperty.BlockDifficulty, "block.difficulty" },
+                { BlockOrTransactionProperty.BlockGaslimit, "block.gaslimit" },
+                { BlockOrTransactionProperty.BlockNumber, "block.number" },
+                { BlockOrTransactionProperty.BlockTimestamp, "block.timestamp" },
+                { BlockOrTransactionProperty.Gasleft, "gasleft()" },
+                { BlockOrTransactionProperty.MsgData, "msg.data" },
+                { BlockOrTransactionProperty.MsgSender, "msg.sender" },
+                { BlockOrTransactionProperty.MsgSig, "msg.sig" },
+                { BlockOrTransactionProperty.MsgValue, "msg.value" },
+                { BlockOrTransactionProperty.Now, "now" },
+                { BlockOrTransactionProperty.TxGasprice, "tx.gasprice" },
+                { BlockOrTransactionProperty.TxOrigin, "tx.origin" }
+            };
+
+            foreach (var entry in appropriateCode)
+            {
+                yield return new object[] { new SpecialValueCall() { PropertyToCall = entry.Key }, entry.Value };
+            }
+        }
+    }
+}
