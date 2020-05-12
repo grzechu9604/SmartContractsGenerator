cac7453b5ecd24badc69e7a0953ad91c3fe103d5
Grzegorz W <grzegorz.wilczynski@student.put.poznan.pl>
2020-05-12 22:31:25 +0200

    SolidityTypes as enum

---
+++ SmartContractsGenerator/Model/Enums/SolidityType.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartContractsGenerator.Model.Enums
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1720:Identifier contains type name", Justification = "It is enum of Solidity types. It must contain type name")]
+    public enum SolidityType
+    {
+        Bool = 1,
+        Int = 2,
+        UInt = 3,
+        Fixed = 4,
+        UFixed = 5,
+        Address = 6,
+        AddressPayable = 7,
+        String = 8
+    }
+}
